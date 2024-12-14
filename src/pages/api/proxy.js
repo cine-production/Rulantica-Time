8b07b69241c097131d41ec0a9d31cd16aa20dfc8
Vischer <adimarcel67@gmail.com>
2024-12-14 22:38:24 +0100

    Initial commit

--- src/pages/api/proxy.js
+++ src/pages/api/proxy.js
@@ -7,20 +7,15 @@ export default function handler(req, res) {
     return res.status(400).json({ error: 'Park ID is required' });
   }
 
-  // Ajouter les en-têtes CORS à la réponse du proxy
-  res.setHeader('Access-Control-Allow-Origin', '*'); // Permet à n'importe quelle origine d'accéder à cette ressource
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-
   // Proxy vers l'API des temps d'attente du parc
   createProxyMiddleware({
-    target: `https://queue-times.com/parks/${parkId}/queue_times.json`,  // URL dynamique selon le parc
-    changeOrigin: true,  // Permet au proxy de faire une demande avec un en-tête "Origin" ajusté
+    target: `https://queue-times.com/parks/${parkId}/queue_times.json`,
+    changeOrigin: true,
     pathRewrite: {
-      '^/api/proxy': '',  // Réécrit le chemin de l'API pour correspondre à l'API cible
+      '^/api/proxy': '',
     },
-    onProxyRes(proxyRes, req, res) {
-      // En-têtes supplémentaires pour autoriser l'accès cross-origin
+    // Configuration des en-têtes CORS pour la réponse du proxy
+    onProxyRes: proxyRes => {
       proxyRes.headers['Access-Control-Allow-Origin'] = '*';
       proxyRes.headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, DELETE';
       proxyRes.headers['Access-Control-Allow-Headers'] = 'Content-Type';
