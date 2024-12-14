9d4677b243c21ff9e1b7715eb8c84f216355091c
Vischer <adimarcel67@gmail.com>
2024-12-14 20:30:27 +0100

    Initial commit

--- src/pages/api/proxy.js
+++ src/pages/api/proxy.js
@@ -1,27 +1,19 @@
 // pages/api/proxy.js
 import { createProxyMiddleware } from 'http-proxy-middleware';
 
-const proxy = createProxyMiddleware({
-  target: 'https://queue-times.com', // URL de base de l'API
-  changeOrigin: true,
-  pathRewrite: {
-    '^/api/proxy': '', // Supprime le préfixe /api/proxy
-  },
-});
-
-// Middleware personnalisé pour gérer le proxy
 export default function handler(req, res) {
-  const { parkId } = req.query; // Récupérer l'ID du parc
+  const { parkId } = req.query; // Récupérer l'ID du parc depuis la requête
 
   if (!parkId) {
     return res.status(400).json({ error: 'Park ID is required' });
   }
 
-  req.url = `/parks/${parkId}/queue_times.json`; // Ajuster le chemin pour le proxy
-  return proxy(req, res, (result) => {
-    if (result instanceof Error) {
-      console.error('Erreur de proxy:', result);
-      res.status(500).json({ error: 'Erreur interne du serveur.' });
-    }
-  });
+  // Proxy vers l'API des temps d'attente du parc
+  return createProxyMiddleware({
+    target: `https://queue-times.com/parks/${parkId}/queue_times.json`, // URL dynamique selon le parc
+    changeOrigin: true,
+    pathRewrite: {
+      '^/api/proxy': '', // Retirer le préfixe '/api/proxy'
+    },
+  })(req, res);
 }
