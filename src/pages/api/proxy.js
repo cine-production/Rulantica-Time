024e7981c732ee17e14503d12677633f3370d007
Vischer <adimarcel67@gmail.com>
2024-12-15 15:41:45 +0100

    Initial commit

--- src/pages/api/proxy.js
+++ src/pages/api/proxy.js
@@ -1,10 +1,13 @@
 // pages/api/proxy.js
 export async function handler(req, res) {
-  const targetUrl = `https://ep-time.vercel.app/api/parks/${parkId}/queue_times.json`;
+  const targetUrl = `https://queue-times.com${req.url}`;
 
   try {
     const response = await fetch(targetUrl);
     const data = await response.json();
+    
+    // Ajout des en-têtes CORS pour permettre l'accès depuis ton domaine
+    res.setHeader('Access-Control-Allow-Origin', '*');  // Permet l'accès depuis n'importe quel domaine
     res.status(200).json(data);
   } catch (error) {
     res.status(500).json({ error: 'Erreur lors de la récupération des données.' });
