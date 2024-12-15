48da70daa2dc58f16bdcb3f3a0fee92c04949a64
Vischer <adimarcel67@gmail.com>
2024-12-15 21:30:29 +0100

    Initial commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -186,7 +186,6 @@ export default function Home() {
             <ContentWrapper message={""}>{actions(state)}</ContentWrapper>
             {result(state)}
             <Links />
-            <SeoText />
             <Disclaimer />
           </div>
         )}
