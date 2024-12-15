01134536719b9bf7422a0750f1951a26fe9bc706
Vischer <adimarcel67@gmail.com>
2024-12-15 15:37:41 +0100

    Initial commit

--- src/pages/api/proxy.js
+++ src/pages/api/proxy.js
@@ -1,6 +1,6 @@
 // pages/api/proxy.js
 export async function handler(req, res) {
-  const targetUrl = `https://queue-times.com${req.url}`;
+  const targetUrl = `https://ep-time.vercel.app/api/parks/${parkId}/queue_times.json`;
 
   try {
     const response = await fetch(targetUrl);
